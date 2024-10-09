b9030d3f51459eb31747100d691cd1e7e90be2aa
neginsadani <sadaninegin@gmail.com>
2024-10-09 19:02:19 +0330

    add all

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const firstParagraph = document.querySelector("p");
+firstParagraph.textContent = "First paragraph has been updated!";
+
+document.querySelector(".myParagraph").style.backgroundColor = "lightblue";
+document.querySelector(".myParagraph1").style.backgroundColor = "lightblue";
+document.querySelector(".myParagraph2").style.backgroundColor = "lightblue";
